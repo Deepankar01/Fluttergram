2095a0db32f44d13bb61eb5fccc45e12f4ddc964
Deepankar Agrawal <deepankar_agrawal@external.mckinsey.com>
2019-09-17 14:05:44 +0530

    refactored

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:fluttergram/Pallete.dart';
+
 import 'package:fluttergram/SocialFeeds/SocialFeeds.dart';
+
+import 'locale/en.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SocialFeeds();
+    return Scaffold(
+      backgroundColor: ColorPallete.white,
+      bottomSheet: Container(
+        color: Colors.red,
+        height: 50,
+        child: Row(
+          children: <Widget>[],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: ColorPallete.white,
+        actions: <Widget>[
+          Container(
+            width: 50,
+            color: Colors.indigo,
+          ),
+          Container(
+            width: 50,
+            color: Colors.green,
+          )
+        ],
+        leading: Container(
+          color: Colors.red,
+        ),
+        centerTitle: false,
+        textTheme:
+            TextTheme(title: TextStyle(color: ColorPallete.blackWithAccent)),
+        title: Text(getMessages()['appName']),
+      ),
+      body: SocialFeeds(),
+    );
   }
 }
--- lib/SocialFeeds/SocialFeeds.dart
+++ lib/SocialFeeds/SocialFeeds.dart
@@ -1,39 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:fluttergram/Pallete.dart';
-import 'package:fluttergram/locale/en.dart';
 
 class SocialFeeds extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorPallete.white,
-      bottomSheet: Container(
-        color: Colors.red,
-        height: 50,
-        child: Row(
-          children: <Widget>[],
-        ),
-      ),
-      appBar: AppBar(
-        backgroundColor: ColorPallete.white,
-        actions: <Widget>[
-          Container(
-            width: 50,
-            color: Colors.indigo,
-          ),
-          Container(
-            width: 50,
-            color: Colors.green,
-          )
-        ],
-        leading: Container(
-          color: Colors.red,
-        ),
-        centerTitle: false,
-        textTheme:
-            TextTheme(title: TextStyle(color: ColorPallete.blackWithAccent)),
-        title: Text(getMessages()['appName']),
-      ),
-    );
+    return Container();
   }
 }
